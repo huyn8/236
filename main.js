05f7ad1dd3961edd6b6a4dbd9b20e1a17a136644
huyn8 <huyn8@uw.edu>
2019-11-17 13:07:42 -0800

    code clean up

--- main.js
+++ main.js
@@ -1,36 +1,7 @@
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function myFunction(num) {
+    document.getElementById(num).classList.toggle("show");
 }
-
-function myFunction2() {
-    document.getElementById("myDropdown2").classList.toggle("show");
-}
-
-function myFunction3() {
-    document.getElementById("myDropdown3").classList.toggle("show");
-}
-
-function myFunction4() {
-    document.getElementById("myDropdown4").classList.toggle("show");
-}
-
-function myFunction5() {
-    document.getElementById("myDropdown5").classList.toggle("show");
-}
-
-function myFunction6() {
-    document.getElementById("myDropdown6").classList.toggle("show");
-}
-
-function myFunction7() {
-    document.getElementById("myDropdown7").classList.toggle("show");
-}
-
-function myFunction8() {
-    document.getElementById("myDropdown8").classList.toggle("show");
-}
-
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
